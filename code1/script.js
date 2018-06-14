9871eaeb20697f5395ef09adc33da71e9eae9377
kaytec@hlpusd.k12.ca.us <kaytec@hlpusd.k12.ca.us>
2018-06-14 16:12:53 -0700

    i did some stuff teehee

---
+++ code1/script.js
@@ -0,0 +1,7 @@
+// used in map-list unordered list
+function changePic(image) {
+  document.getElementById("full-map-image").src = image;
+}
+// for descripions of maps idk how to do this
+//function changePicDesc() {
+//}
