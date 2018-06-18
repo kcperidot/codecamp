fe3b95e1f3578aa71a180c93bc9c734998171902
kaytec@hlpusd.k12.ca.us <kaytec@hlpusd.k12.ca.us>
2018-06-18 16:04:10 -0700

    pictures and other stuff

--- code1/script.js
+++ code1/script.js
@@ -1,7 +1,22 @@
+//faq links
+var home = document.getElementById("home");
+home.addEventListener('click', function(){
+//insert content here (link to other page)
+})
+var news = document.getElementById("news");
+news.addEventListener('click', function(){
+//insert content here
+})
+var faq = document.getElementById("faq");
+faq.addEventListener('click', function(){
+//insert content here
+})
+
 // used in map-list unordered list
 function changePic(image) {
   document.getElementById("full-map-image").src = image;
 }
+
 // for descripions of maps idk how to do this
-//function changePicDesc() {
-//}
+var mapdesc = document.getElementById("mapdesc");
+//if (document.getElementById("full-map-image").src == ){}
