e120c512291c6855bdde3ebb3f6aa23fd24558bd
sarahkwon01 <40196235+sarahkwon01@users.noreply.github.com>
2018-06-19 18:17:32 -0700

    gsgwesgersgwegwesg
    if i said in a comment bad code below plz fix, im like 80% sure i messed up somewhere and changing something screws everything up hahahaahhaa
    
    also everytime i use pixels it just means i have no idea how to measure this

---
+++ codecamp-master/README.md
@@ -0,0 +1,3 @@
+"# codecamp" 
+"# codecamp" 
+"# codecamp" 
---
+++ codecamp-master/code1/script.js
@@ -0,0 +1,24 @@
+//faq links
+var home = document.getElementById("home");
+home.addEventListener('click', function(){
+//insert content here (link to other page)
+})
+var news = document.getElementById("news");
+news.addEventListener('click', function(){
+//insert content here
+})
+var faq = document.getElementById("faq");
+faq.addEventListener('click', function(){
+//insert content here
+})
+
+// used in map-list unordered list
+function changePic(image) {
+  document.getElementById("full-map-image").src = image;
+}
+
+
+
+// for descripions of maps idk how to do this
+var mapdesc = document.getElementById("mapdesc");
+//if (document.getElementById("full-map-image").src == ){}
